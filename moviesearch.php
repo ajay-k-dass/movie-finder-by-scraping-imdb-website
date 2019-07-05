701334f053133bd5a44efe309f3f3720532a0075
ajay-k-dass <38295420+ajay-k-dass@users.noreply.github.com>
2019-07-05 15:41:01 +0530

    Update moviesearch.php

--- moviesearch.php
+++ moviesearch.php
@@ -1,34 +1,36 @@
-<!DOCTYPE html>
-<html>
-
-<body>
-<form action="" method="post">
-
-  <input name="search" type="search" autofocus><input type="submit" name="button">
-
-</form>
-
-</body>
-</html>
 <?php
+
  $dbhost = "localhost";
  $dbuser = "localhost";
  $dbpass = "###123###";
- $db = "csv_db";
- $conn = new mysqli($dbhost, $dbuser, $dbpass,$db);
- echo "success";
- if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-
-  $searchString =  $_POST["search"];
-    $results = mysqli_query("SELECT * FROM tbl_name WHERE title LIKE %ss", $searchString);
-    echo($results);
-  
-
-
-
-
-
+$con=mysqli_connect($dbhost, $dbuser, $dbpass) or die("Connect failed:".mysqli_connect_error());
+mysqli_select_db($con,"csv_db") or die (mysqli_connect_error());
+mysqli_close($con);
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Search results</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+<?php
+    $query = $_GET['query']; 
+         
+        $query = htmlspecialchars($query); 
+         
+        $query = mysqli_real_escape_string($con,$query);
+        $raw_results = mysqli_query($con,"SELECT * FROM tbl_name
+            WHERE ('COL 2' LIKE '%".$query."%') ") or die(mysqli_error());
+             if(mysqli_num_rows($con,$raw_results) > 0){ 
+             while($results = mysqli_fetch_array($con,$raw_results)){
+                       echo "<p>".$results['COL 2']."</p>";
+                }
+             }
+        else{ 
+            echo "No results";
+        }
+?>
+</body>
+</html>
