aa57b65787ae1fc306326ebf688433014faa38b3
ajay-k-dass <38295420+ajay-k-dass@users.noreply.github.com>
2019-07-05 15:42:01 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+      <title>movie search</title>
+</head>
+<body>
+<form action="search.php" method="GET">
+    <input type="text" name="query" />
+    <input type="submit" value="Search" />
+</form>
+</body>
